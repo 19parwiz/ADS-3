43df5089f850be1936ebdda6ef1a1abf6f715d8a
Ali Parwiz <232141@astanait.edu.kz>
2024-05-10 00:29:25 +0600

    Create the "My Hashtabel" Second: Implemening some method Third commit

---
+++ src/BST.java
@@ -0,0 +1,162 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class BST<K extends Comparable<K>, V> implements Iterable<K> {
+    private class Node {
+        private K key;
+        private V value;
+        private Node left, right;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private Node root;
+
+    public BST() {
+        root = null;
+    }
+
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+
+    private Node put(Node node, K key, V value) {
+        if (node == null) {
+            return new Node(key, value);
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, value);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, value);
+        } else {
+            node.value = value; // Update value if key already exists
+        }
+        return node;
+    }
+
+    public V get(K key) {
+        Node node = get(root, key);
+        return node == null ? null : node.value;
+    }
+
+    private Node get(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return get(node.left, key);
+        } else if (cmp > 0) {
+            return get(node.right, key);
+        } else {
+            return node;
+        }
+    }
+
+    public void delete(K key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = delete(node.left, key);
+        } else if (cmp > 0) {
+            node.right = delete(node.right, key);
+        } else {
+            if (node.left == null) {
+                return node.right;
+            } else if (node.right == null) {
+                return node.left;
+            } else {
+                Node minNode = findMin(node.right);
+                node.key = minNode.key;
+                node.value = minNode.value;
+                node.right = deleteMin(node.right);
+            }
+        }
+        return node;
+    }
+
+    private Node findMin(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return findMin(node.left);
+    }
+
+    private Node deleteMin(Node node) {
+        if (node.left == null) {
+            return node.right;
+        }
+        node.left = deleteMin(node.left);
+        return node;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return new InOrderIterator();
+    }
+
+    private class InOrderIterator implements Iterator<K> {
+        private Node current;
+        private java.util.Stack<Node> stack;
+
+        public InOrderIterator() {
+            current = root;
+            stack = new java.util.Stack<>();
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public K next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("No more elements in the BST");
+            }
+            Node node = stack.pop();
+            K key = node.key;
+            if (node.right != null) {
+                node = node.right;
+                while (node != null) {
+                    stack.push(node);
+                    node = node.left;
+                }
+            }
+            return key;
+        }
+    }
+
+    public static void main(String[] args) {
+        BST<Integer, String> tree = new BST<>();
+        tree.put(5, "Five");
+        tree.put(3, "Three");
+        tree.put(7, "Seven");
+        tree.put(2, "Two");
+        tree.put(4, "Four");
+
+        System.out.println("Value for key 3: " + tree.get(3));
+
+        System.out.println("Iterating through the BST:");
+        for (int key : tree) {
+            System.out.println("Key: " + key + ", Value: " + tree.get(key));
+        }
+    }
+}
